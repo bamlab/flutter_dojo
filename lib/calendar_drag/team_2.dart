749a6cd932d4888fef903763e825a716abd96c9e
Lulupointu <lucasdelsol@hotmail.fr>
2022-04-26 12:47:35 +0200

    Team cs 2022

--- lib/calendar_drag/team_2.dart
+++ lib/calendar_drag/team_2.dart
@@ -1,6 +1,8 @@
 import 'package:bam_dojo/helpers/team_class.dart';
 import 'package:flutter/material.dart';
 
+import 'calendar_drag.dart';
+
 class CalendarTeam2 extends TeamWidget {
   const CalendarTeam2({
     Key? key,
@@ -8,6 +10,160 @@ class CalendarTeam2 extends TeamWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text(teamName)));
+    return Scaffold(
+      body: CalendarTeam(),
+    );
+  }
+}
+
+class CalendarTeam extends StatefulWidget {
+  const CalendarTeam({
+    Key? key,
+  }) : super(
+          key: key,
+        );
+
+  @override
+  State<CalendarTeam> createState() => _CalendarTeamState();
+}
+
+class _CalendarTeamState extends State<CalendarTeam> {
+  @override
+  Widget build(BuildContext context) {
+    const eventHeight = 80.0;
+    return Scaffold(
+      body: Center(
+        // Show this events in a column
+        child: _CreateEventHandler(
+          height: eventHeight,
+          child: Container(
+            color: Colors.blue,
+            height: eventHeight,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _CreateEventHandler extends StatefulWidget {
+  const _CreateEventHandler(
+      {Key? key, required this.child, required this.height})
+      : super(key: key);
+
+  final Widget child;
+  final double height;
+
+  @override
+  State<_CreateEventHandler> createState() => _CreateEventHandlerState();
+}
+
+class _CreateEventHandlerState extends State<_CreateEventHandler> {
+  var _isDragging = false;
+  var _initialPosTop = 0.0;
+  var _initialPosBottom = 0.0;
+  double? maybeTopPos = null;
+  double? maybeBottomPos = null;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onVerticalDragStart: (details) {
+        setState(() {
+          _isDragging = true;
+        });
+        _initialPosTop = details.localPosition.dy;
+        _initialPosBottom = details.localPosition.dy;
+      },
+      onVerticalDragUpdate: (details) {
+        setState(() {
+          if (details.localPosition.dy > _initialPosBottom) {
+            maybeTopPos = null;
+            maybeBottomPos = details.localPosition.dy;
+          } else if (details.localPosition.dy < _initialPosTop) {
+            maybeTopPos = details.localPosition.dy;
+            maybeBottomPos = null;
+          } else {
+            maybeTopPos = null;
+            maybeBottomPos = null;
+          }
+        });
+      },
+      onVerticalDragEnd: (details) {
+        setState(() {
+          _isDragging = false;
+          _initialPosTop = 0.0;
+          _initialPosBottom = 0.0;
+          maybeTopPos = null;
+          maybeBottomPos = null;
+        });
+      },
+      child: Stack(
+        children: [
+          widget.child,
+          if (_isDragging)
+            Positioned(
+              top: maybeTopPos ?? _initialPosTop,
+              height: (maybeBottomPos ?? _initialPosBottom) -
+                  (maybeTopPos ?? _initialPosTop)                  ,
+              left: 0,
+              right: 0,
+              child: Container(
+                color: Colors.red,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+class EventWidget extends StatelessWidget {
+  const EventWidget({
+    Key? key,
+    required this.event,
+  }) : super(key: key);
+
+  final Event event;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: event.color.withOpacity(0.1),
+        borderRadius: BorderRadius.circular(10),
+      ),
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: 5,
+            decoration: BoxDecoration(
+              color: event.color,
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+            ),
+          ),
+          const SizedBox(width: 8),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Text(
+                event.name,
+                style:
+                    const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              Text(
+                event.startDate.hour.toString() +
+                    ' - ' +
+                    event.endDate.hour.toString(),
+                style: const TextStyle(fontSize: 15, color: Colors.grey),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/calendar_responsive/team_2.dart
+++ lib/calendar_responsive/team_2.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:bam_dojo/helpers/team_class.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +10,96 @@ class CalendarTeam2 extends TeamWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text(teamName)));
+    return const Scaffold(
+      body: Center(
+        child: _ResponsiveCalendar(),
+      ),
+    );
+  }
+}
+
+class _ResponsiveCalendar extends StatefulWidget {
+  const _ResponsiveCalendar({Key? key}) : super(key: key);
+
+  @override
+  State<_ResponsiveCalendar> createState() => _ResponsiveCalendarState();
+}
+
+class _ResponsiveCalendarState extends State<_ResponsiveCalendar> {
+  final minWidth = 200;
+  var isFirst = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WidgetsBinding.instance!.addPostFrameCallback((timeStamp) {
+      isFirst = false;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final numberOfDays = constraints.maxWidth ~/ minWidth;
+        final dayWidth = constraints.maxWidth / numberOfDays;
+
+        return Row(
+          children: [
+            for (var i = 0; i < numberOfDays; i++)
+              _CalendarDay(
+                key: ValueKey(i),
+                width: dayWidth,
+                animatedFromZero: !isFirst,
+              ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class _CalendarDay extends StatefulWidget {
+  const _CalendarDay({
+    Key? key,
+    required this.width,
+    required this.animatedFromZero,
+  }) : super(key: key);
+
+  final double width;
+
+  final bool animatedFromZero;
+
+  @override
+  State<_CalendarDay> createState() => _CalendarDayState();
+}
+
+class _CalendarDayState extends State<_CalendarDay> {
+  final _color = Color.fromARGB(
+    255,
+    Random().nextInt(255),
+    Random().nextInt(255),
+    Random().nextInt(255),
+  );
+
+  late final double _beginWidth = widget.animatedFromZero ? 0 : widget.width;
+
+  @override
+  Widget build(BuildContext context) {
+    return TweenAnimationBuilder<double>(
+      duration: const Duration(milliseconds: 100),
+      tween: Tween(
+        begin: _beginWidth,
+        end: widget.width,
+      ),
+      builder: (context, width, child) {
+        return Container(
+          width: width,
+          height: double.infinity,
+          color: _color,
+        );
+      },
+    );
   }
 }
