419df704cc8e5df517773f91dcfbe6728a92b2f9
Lulupointu <lucasdelsol@hotmail.fr>
2022-07-19 14:41:49 +0200

    End like implementation

--- lib/animated_likes/team_3.dart
+++ lib/animated_likes/team_3.dart
@@ -12,21 +12,76 @@ class AnimatedLikesTeam3 extends TeamWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: _AnimatedIcon(),
+      child: _ClickableAnimatedButton(),
+    );
+  }
+}
+
+class _ClickableAnimatedButton extends StatefulWidget {
+  const _ClickableAnimatedButton({Key? key}) : super(key: key);
+
+  @override
+  State<_ClickableAnimatedButton> createState() =>
+      _ClickableAnimatedButtonState();
+}
+
+class _ClickableAnimatedButtonState extends State<_ClickableAnimatedButton> {
+  final _animatedIconsKeys = <Key>[];
+  final size = 100.0;
+  late final icon = Icon(
+    Icons.favorite,
+    color: Color(0xFFFD5588),
+    size: size,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Stack(
+        children: [
+          ..._animatedIconsKeys.map(
+            (key) => _AnimatedIcon(key: key, icon: icon, size: size,),
+          ),
+          GestureDetector(
+            onTap: () async {
+              setState(() {
+                _animatedIconsKeys.add(UniqueKey());
+              });
+              await Future.delayed(Duration(seconds: 1));
+              if (mounted) {
+                _animatedIconsKeys.remove(UniqueKey());
+              }
+            },
+            child: icon,
+          ),
+        ],
+      ),
     );
   }
 }
 
 class _AnimatedIcon extends StatefulWidget {
-  const _AnimatedIcon({Key? key}) : super(key: key);
+  const _AnimatedIcon({
+    Key? key,
+    required this.icon,
+    required this.size,
+  }) : super(key: key);
+
+  final Widget icon;
+  final double size;
 
   @override
-  State<_AnimatedIcon> createState() => __AnimatedIconState();
+  State<_AnimatedIcon> createState() => _AnimatedIconState();
 }
 
-class __AnimatedIconState extends State<_AnimatedIcon> {
+class _AnimatedIconState extends State<_AnimatedIcon> {
   int plusOrMinusX = 1;
   var continueTimer = true;
+
+  // The final y position of the icon
+  late final endYOffset = -(1.5 + Random().nextDouble()) * widget.size;
+
+  final animationDuration = Duration(seconds: 1);
 
   @override
   void initState() {
@@ -35,12 +90,14 @@ class __AnimatedIconState extends State<_AnimatedIcon> {
   }
 
   Future<void> animateOnX() async {
-    final duration = Duration(milliseconds: (100 + Random().nextInt(100)));
     while (continueTimer) {
+      final duration = Duration(milliseconds: (100 + Random().nextInt(100)));
       await Future.delayed(duration);
-      setState(() {
-        plusOrMinusX = plusOrMinusX * -1;
-      });
+      if (mounted) {
+        setState(() {
+          plusOrMinusX = plusOrMinusX * -1;
+        });
+      }
     }
   }
 
@@ -55,33 +112,44 @@ class __AnimatedIconState extends State<_AnimatedIcon> {
 
   @override
   Widget build(BuildContext context) {
-    final size = 100.0;
-
     return Transform.rotate(
       angle: originAngle,
       child: TweenAnimationBuilder<double>(
-          tween: Tween(begin: 0.0, end: plusOrMinusX * 0.5 * size),
-          duration: Duration(milliseconds: 100),
-          builder: (context, xOffset, child) {
-            return TweenAnimationBuilder<double>(
-              tween: Tween(begin: 0.0, end: 1.0),
-              duration: Duration(seconds: 1),
-              builder: (context, value, child) {
-                final yOffset = Tween(begin: 0.0, end: -2 * size).evaluate(
-                  AlwaysStoppedAnimation(value),
-                );
+        tween: Tween(begin: 0.0, end: plusOrMinusX * 0.5 * widget.size),
+        duration: Duration(milliseconds: 100),
+        builder: (context, xOffset, child) {
+          return TweenAnimationBuilder<double>(
+            tween: Tween(begin: 0.0, end: endYOffset),
+            duration: animationDuration,
+            builder: (context, yOffset, child) {
+              return TweenAnimationBuilder<double>(
+                tween: Tween(begin: 0.0, end: 1.0),
+                duration: animationDuration,
+                builder: (context, progress, child) {
+                  final fadeStartProgress = 0.5;
+                  final fadeDuration =
+                      (animationDuration.inMilliseconds * fadeStartProgress)
+                          .ceil();
 
-                return Transform.translate(
+                  return AnimatedOpacity(
+                    duration: Duration(milliseconds: fadeDuration),
+                    opacity: progress > fadeStartProgress ? 0 : 1,
+                    child: child!,
+                  );
+                },
+                child: Transform.translate(
                   offset: Offset(xOffset, yOffset),
                   child: Icon(
                     Icons.favorite,
                     color: Color(0xFFFD5588),
-                    size: size,
+                    size: widget.size,
                   ),
-                );
-              },
-            );
-          }),
+                ),
+              );
+            },
+          );
+        },
+      ),
     );
   }
 }
