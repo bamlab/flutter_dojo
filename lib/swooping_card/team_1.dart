199c354ad8fb5b881b89879a9bdc9feb21d28ba4
louisdachet <louis@dachet.fr>
2022-11-08 12:04:54 +0100

    feat(swooping_card): improve code

--- lib/swooping_card/team_1.dart
+++ lib/swooping_card/team_1.dart
@@ -9,19 +9,47 @@ class SwoopingCardTeam1 extends TeamWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(children: [_Card()]),
+      body: Stack(
+        children: [
+          _PositionedCard(
+            xAlignment: 1,
+            enableAnimation: false,
+            imageUrl: 'assets/images/card1.png',
+          ),
+          _PositionedCard(
+            xAlignment: 0,
+            enableAnimation: true,
+            imageUrl: 'assets/images/card2.png',
+          ),
+          _PositionedCard(
+            xAlignment: -1,
+            enableAnimation: false,
+            imageUrl: 'assets/images/card3.png',
+          ),
+        ],
+      ),
     );
   }
 }
 
-class _Card extends StatefulWidget {
-  const _Card({Key? key}) : super(key: key);
+class _PositionedCard extends StatefulWidget {
+  const _PositionedCard({
+    Key? key,
+    required this.xAlignment,
+    required this.enableAnimation,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  final double xAlignment;
+  final bool enableAnimation;
+  final String imageUrl;
 
   @override
-  State<_Card> createState() => _CardState();
+  State<_PositionedCard> createState() => _PositionedCardState();
 }
 
-class _CardState extends State<_Card> with SingleTickerProviderStateMixin {
+class _PositionedCardState extends State<_PositionedCard>
+    with SingleTickerProviderStateMixin {
   late final animationController = AnimationController(
     vsync: this,
     duration: Duration(milliseconds: 300),
@@ -29,11 +57,11 @@ class _CardState extends State<_Card> with SingleTickerProviderStateMixin {
 
   double xMapper(double x) {
     final translatedX = (x * 2 - 1);
-    return (-translatedX * translatedX + 1) * 0.2;
+    return ((-translatedX * translatedX + 1) * 0.2) + widget.xAlignment;
   }
 
   final yTween =
-      Tween(begin: 0.8, end: 0.0).chain(CurveTween(curve: Curves.easeInOut));
+      Tween(begin: 0.8, end: -0.2).chain(CurveTween(curve: Curves.easeInOut));
 
   void initState() {
     super.initState();
@@ -41,7 +69,11 @@ class _CardState extends State<_Card> with SingleTickerProviderStateMixin {
   }
 
   _updatePosition() {
-    animationController.forward();
+    widget.enableAnimation
+        ? animationController.isCompleted
+            ? animationController.reverse()
+            : animationController.forward()
+        : null;
   }
 
   @override
@@ -55,12 +87,37 @@ class _CardState extends State<_Card> with SingleTickerProviderStateMixin {
             xMapper(animationController.value),
             yTween.transform(animationController.value),
           ),
-          child: Card(
-            child: SizedBox.square(
-              dimension: 39,
-            ),
-            color: Colors.redAccent,
+          child: _Card(
+            imageUrl: widget.imageUrl,
           ),
+        ),
+      ),
+    );
+  }
+}
+
+class _Card extends StatelessWidget {
+  const _Card({
+    Key? key,
+    required this.imageUrl,
+  }) : super(key: key);
+
+  final String imageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+      ),
+      clipBehavior: Clip.antiAlias,
+      elevation: 8,
+      child: SizedBox(
+        width: 100,
+        height: 150,
+        child: Image.asset(
+          imageUrl,
+          fit: BoxFit.cover,
         ),
       ),
     );
