e5849d107c4e0bcc2ac20db4abd2e1ae72a6259c
Lulupointu <lucas.delsol@contentsquare-ext.com>
2023-04-11 11:34:10 +0200

    RefreshPlus: First implementation

--- lib/refresh_plus/team_1.dart
+++ lib/refresh_plus/team_1.dart
@@ -1,6 +1,8 @@
 import 'package:bam_dojo/helpers/team_class.dart';
 import 'package:flutter/material.dart';
 
+import 'dart:ui' as ui;
+
 class RefreshPlusTeam1 extends TeamWidget {
   const RefreshPlusTeam1({
     Key? key,
@@ -8,6 +10,122 @@ class RefreshPlusTeam1 extends TeamWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox.expand();
+    return _AnimatedCircles();
+  }
+}
+
+class _AnimatedCircles extends StatefulWidget {
+  @override
+  _AnimatedCirclesState createState() => _AnimatedCirclesState();
+}
+
+class _AnimatedCirclesState extends State<_AnimatedCircles>
+    with SingleTickerProviderStateMixin {
+  bool animate = false;
+  late AnimationController _controller;
+
+  var touchPoint = Offset.zero;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller =
+        AnimationController(duration: Duration(seconds: 2), vsync: this)
+          ..repeat(reverse: true);
+    _controller.addListener(() {
+      setState(() {});
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SizedBox(
+          height: 400,
+          width: 400,
+          child: ColorFiltered(
+            colorFilter: ColorFilter.matrix(
+              [
+                1, 0, 0, 0, 0, //
+                0, 1, 0, 0, 0, //
+                0, 0, 1, 0, 0, //
+                0, 0, 0, 255, 0, //
+              ],
+            ),
+            child: ColorFiltered(
+              colorFilter: ColorFilter.matrix(
+                [
+                  1, 0, 0, 0, 0, //
+                  0, 1, 0, 0, 0, //
+                  0, 0, 1, 0, 0, //
+                  0, 0, 0, 1, -255 / 2, //
+                ],
+              ),
+              child: GestureDetector(
+                onPanUpdate: (details) {
+                  setState(() {
+                    touchPoint = details.localPosition;
+                  });
+                },
+                child: CustomPaint(
+                  painter: AnimatedCirclesPainter(touchPoint),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class AnimatedCirclesPainter extends CustomPainter {
+  final Offset position;
+
+  AnimatedCirclesPainter(this.position);
+
+  @override
+  void paint(Canvas canvas, Size size) {
+    // Create a paint for the circles
+    final paint = Paint()..color = Colors.black;
+    // paint.shader = shader;
+
+    // Create a rect to use as a clip rect for the blur
+    final rect = Rect.fromLTWH(0, 0, size.width, size.height);
+
+    // Add a gaussian blur filter
+    final blurFilter = ui.ImageFilter.blur(sigmaX: 15, sigmaY: 15);
+    canvas.saveLayer(rect, Paint()..imageFilter = blurFilter);
+
+    // Draw the circles in a layer
+    canvas.saveLayer(rect, Paint());
+    canvas.drawCircle(
+      Offset(size.width / 2, size.height / 2),
+      size.width / 8,
+      paint,
+    );
+    canvas.drawCircle(
+      position,
+      size.width / 8,
+      paint,
+    );
+
+    // Restore the layers
+    canvas.restore();
+    canvas.restore();
+    // canvas.restore();
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
   }
 }
