09c52fbe282521df25b260f64338d194de56bab4
Lulupointu <lucas.delsol@contentsquare-ext.com>
2023-04-11 13:54:06 +0200

    IconMorph: First implem

--- lib/icon_morph/team_3.dart
+++ lib/icon_morph/team_3.dart
@@ -1,4 +1,5 @@
 import 'package:bam_dojo/helpers/team_class.dart';
+import 'package:bam_dojo/hero_list/team_1.dart';
 import 'package:flutter/material.dart';
 
 class IconMorphTeam3 extends TeamWidget {
@@ -17,10 +18,11 @@ class _IconsSelector extends StatefulWidget {
   _IconsSelectorState createState() => _IconsSelectorState();
 }
 
-class _IconsSelectorState extends State<_IconsSelector> {
+class _IconsSelectorState extends State<_IconsSelector>
+    with TickerProviderStateMixin {
   final _selectableIcons = [
-    Icons.ac_unit,
-    Icons.access_time,
+    Icons.square,
+    Icons.circle,
     Icons.add,
     Icons.add_a_photo,
     Icons.add_alert,
@@ -32,18 +34,51 @@ class _IconsSelectorState extends State<_IconsSelector> {
   ];
 
   late var _selectedIcon = _selectableIcons.first;
+  IconData? _previouslySelectedIcon;
+
+  final animationDuration = Duration(milliseconds: 1000);
+
+  late final blurAnimationController = AnimationController(
+    vsync: this,
+    duration: animationDuration,
+  );
+
+  @override
+  void initState() {
+    super.initState();
+
+    blurAnimationController.addListener(() {
+      setState(() {});
+    });
+  }
 
   Widget buildIconSelector({required IconData icon}) {
     return _IconSelector(
       icon: icon,
       selected: _selectedIcon == icon,
-      onTap: () => setState(() => _selectedIcon = icon),
+      onTap: () async {
+        await blurAnimationController.forward();
+
+        if (mounted) {
+          setState(() {
+            _previouslySelectedIcon = _selectedIcon;
+            _selectedIcon = icon;
+          });
+        }
+
+        // Wait for the morph
+        await animationDuration;
+
+        // unblur
+        await blurAnimationController.reverse();
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    // TODO: Morph between icons. Get inspiration from lib/refresh_plus/team_1.dart
+    final previouslySelectedIcon = _previouslySelectedIcon;
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Icon Morph'),
@@ -52,7 +87,58 @@ class _IconsSelectorState extends State<_IconsSelector> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Icon(_selectedIcon, size: 100),
+            ColorFiltered(
+              colorFilter: ColorFilter.matrix(
+                [
+                  1, 0, 0, 0, 0, //
+                  0, 1, 0, 0, 0, //
+                  0, 0, 1, 0, 0, //
+                  0, 0, 0, 255, 0, //
+                ],
+              ),
+              child: ColorFiltered(
+                colorFilter: ColorFilter.matrix(
+                  [
+                    1, 0, 0, 0, 0, //
+                    0, 1, 0, 0, 0, //
+                    0, 0, 1, 0, 0, //
+                    0, 0, 0, 1, -255 / 2, //
+                  ],
+                ),
+                child: BlurredWidget(
+                  blur: blurAnimationController.value * 10,
+                  child: Stack(
+                    children: [
+                      if (previouslySelectedIcon != null)
+                        TweenAnimationBuilder(
+                          key: ValueKey(previouslySelectedIcon),
+                          duration: animationDuration,
+                          tween: Tween<double>(begin: 1, end: 0),
+                          builder: (context, value, child) {
+                            return Opacity(
+                              opacity: value,
+                              child: child,
+                            );
+                          },
+                          child: Icon(previouslySelectedIcon, size: 100),
+                        ),
+                      TweenAnimationBuilder(
+                        key: ValueKey(_selectedIcon),
+                        duration: animationDuration,
+                        tween: Tween<double>(begin: 0, end: 1),
+                        builder: (context, value, child) {
+                          return Opacity(
+                            opacity: value,
+                            child: child,
+                          );
+                        },
+                        child: Icon(_selectedIcon, size: 100),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
             SizedBox(height: 40),
             GridView(
               padding: EdgeInsets.all(10),
