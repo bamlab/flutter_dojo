f99b76a4980381cf22a4409bb059732857b5b8b8
Lulupointu <lucas.delsol@contentsquare-ext.com>
2023-04-18 19:23:04 +0200

    feat: :sparkles: dropdown_picker: first implementation

--- lib/dropdown_picker/team_3.dart
+++ lib/dropdown_picker/team_3.dart
@@ -9,8 +9,101 @@ class DropdownPickerTeam3 extends StatefulWidget with TeamMixin {
 }
 
 class _DropdownPickerTeam3State extends State<DropdownPickerTeam3> {
+  final items = ['Easy', 'Medium', 'Hard', 'Expert'];
+
+  late var _selectedItem = items.first;
+  var isExpanded = true;
+  final itemHeight = 50.0;
+
   @override
   Widget build(BuildContext context) {
-    return SizedBox.shrink();
+    final translationToTop = itemHeight * (items.length / 2 - 1 / 2);
+    final translationToCurrentItem =
+        translationToTop - itemHeight * items.indexOf(_selectedItem);
+
+    final totalHeight = itemHeight * items.length;
+
+    return Material(
+      color: Colors.black,
+      child: Center(
+        child: TweenAnimationBuilder(
+          duration: const Duration(milliseconds: 300),
+          tween: Tween<double>(
+            begin: 0,
+            end: isExpanded ? translationToCurrentItem : 0,
+          ),
+          builder: (context, translationValue, child) {
+            return Transform.translate(
+              offset: Offset(0, translationValue),
+              child: child,
+            );
+          },
+          child: SizedBox(
+            width: totalHeight,
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 300),
+              height: isExpanded ? totalHeight : itemHeight,
+              clipBehavior: Clip.antiAlias,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: OverflowBox(
+                alignment: Alignment(
+                  0,
+                  2 / 3 * items.indexOf(_selectedItem) - 1,
+                ),
+                maxHeight: double.infinity,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: items
+                      .map(
+                        (item) => _PickerItem(
+                          title: item,
+                          height: itemHeight,
+                          isSelected: item == _selectedItem,
+                          onTap: () {
+                            setState(() {
+                              _selectedItem = item;
+                              isExpanded = !isExpanded;
+                            });
+                          },
+                        ),
+                      )
+                      .toList(),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _PickerItem extends StatelessWidget {
+  _PickerItem({
+    required this.title,
+    required this.height,
+    required this.isSelected,
+    required this.onTap,
+  });
+
+  final String title;
+  final double height;
+  final bool isSelected;
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        height: height,
+        color: isSelected ? Colors.blueGrey : Colors.grey,
+        child: Center(
+          child: Text(title),
+        ),
+      ),
+    );
   }
 }
