2fe2708d0b04406ea13145404ef774307ff07311
giboin <louisg@bam.tech>
2023-05-02 15:54:27 +0200

    waves team1

--- lib/rippling_wave/team_1.dart
+++ lib/rippling_wave/team_1.dart
@@ -9,11 +9,49 @@ class RipplingWaveTeam1 extends StatefulWidget with TeamMixin {
 }
 
 class _RipplingWaveTeam1State extends State<RipplingWaveTeam1> {
+  final columnCount = 30;
+  final rowCount = 50;
+
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: Colors.black,
-      child: Container(),
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+          child: GridView.count(
+        physics: NeverScrollableScrollPhysics(),
+        crossAxisCount: columnCount,
+        children: List.generate(
+          rowCount * columnCount,
+          (index) {
+            return GestureDetector(
+              behavior: HitTestBehavior.opaque,
+              onTap: () {
+                final coordinates = indexToCoordinates(index);
+                print("==> x: ${coordinates.first}, y:${coordinates.last}");
+              },
+              child: AnimatedScale(
+                duration: const Duration(milliseconds: 400),
+                scale: 0.7, //make this vary to see the effect
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      )),
     );
+  }
+
+  int coordinatesToIndex(int x, int y) {
+    //index of (x,y) the grid (0,0 is top left)
+    return y * columnCount + x;
+  }
+
+  List<int> indexToCoordinates(int n) {
+    return [n % columnCount, n ~/ columnCount];
   }
 }
