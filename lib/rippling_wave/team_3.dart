43e3cc1b14a28dc7cffaf1fe3b17aec9c8927c83
remax21 <remid@bam.tech>
2023-05-02 12:36:26 +0200

    add wave animation

--- lib/rippling_wave/team_3.dart
+++ lib/rippling_wave/team_3.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:bam_dojo/helpers/team_class.dart';
 import 'package:flutter/material.dart';
 
@@ -9,16 +12,41 @@ class RipplingWaveTeam3 extends StatefulWidget with TeamMixin {
 }
 
 class _RipplingWaveTeam3State extends State<RipplingWaveTeam3> {
+  Timer? _timer;
+  int time = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _timer = Timer.periodic(const Duration(milliseconds: 1), (timer) {
+      setState(() {
+        time++;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
       color: Colors.black,
       child: GridView.builder(
-          gridDelegate:
-              SliverGridDelegateWithMaxCrossAxisExtent(maxCrossAxisExtent: 20),
-          itemBuilder: (_, __) => DecoratedBox(
-                decoration: BoxDecoration(shape: BoxShape.circle),
-              )),
+        physics: NeverScrollableScrollPhysics(),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 20, crossAxisSpacing: 10, mainAxisSpacing: 10),
+        itemBuilder: (_, index) => Transform.scale(
+          scale: simpleCircularWavePropagation(index % 20, index ~/ 20, time)
+              .toDouble(),
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: index == 20 ? Colors.white : Colors.red),
+          ),
+        ),
+      ),
     );
   }
+}
+
+int simpleCircularWavePropagation(int x, int y, int t) {
+  return max(2 * cos(t - x).toInt(), 1);
 }
