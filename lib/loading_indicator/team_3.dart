97a051a542b7374aeb67b8b8b28dfd57e78dc6ff
MaximeRougieux <maximer@bam.tech>
2023-06-06 11:23:50 +0200

    feat: :sparkles: Sync Loading Indicator team2/3

--- lib/loading_indicator/team_3.dart
+++ lib/loading_indicator/team_3.dart
@@ -16,10 +16,25 @@ class _LoadingIndicatorTeam3State extends State<LoadingIndicatorTeam3> {
   Widget build(BuildContext context) {
     return Material(
       child: Center(
-        child: GradientCircle(
-          color1: g1c1,
-          color2: g1c2,
-          diameter: 200,
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            GradientCircle(
+              color1: g1c1,
+              color2: g1c2,
+              diameter: 100,
+            ),
+            GradientCircle(
+              color1: g2c1,
+              color2: g2c2,
+              diameter: 100,
+            ),
+            GradientCircle(
+              color1: g3c1,
+              color2: g3c2,
+              diameter: 100,
+            ),
+          ],
         ),
       ),
     );
@@ -30,9 +45,24 @@ class GradientCircle extends StatelessWidget {
   final Color color1;
   final Color color2;
 
+  final Tween<Offset> tween1 = Tween(
+    begin: Offset(0, -30),
+    end: Offset(-30, 30),
+  );
+
+  final Tween<Offset> tween2 = Tween(
+    begin: Offset(-30, 30),
+    end: Offset(30, 30),
+  );
+
+  final Tween<Offset> tween3 = Tween(
+    begin: Offset(30, 30),
+    end: Offset(0, -30),
+  );
+
   final double diameter;
 
-  const GradientCircle({
+  GradientCircle({
     super.key,
     required this.color1,
     required this.color2,
@@ -41,18 +71,33 @@ class GradientCircle extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: diameter,
-      width: diameter,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(diameter / 2),
-        gradient: LinearGradient(
-          colors: [
-            color1,
-            color2,
-          ],
-        ),
-      ),
-    );
+    return TweenAnimationBuilder(
+        duration: Duration(seconds: 1),
+        tween: tween2,
+        builder: (context, off2, child) {
+          return TweenAnimationBuilder(
+            duration: Duration(seconds: 1),
+            tween: tween1,
+            builder: (context, off1, child) {
+              return Transform.translate(
+                offset: off1,
+                child: child,
+              );
+            },
+            child: Container(
+              height: diameter,
+              width: diameter,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(diameter / 2),
+                gradient: LinearGradient(
+                  colors: [
+                    color1,
+                    color2,
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
   }
 }
