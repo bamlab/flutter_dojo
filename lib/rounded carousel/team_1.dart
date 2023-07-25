e7df62efacb9745c757191cbd9133b78fdae6cda
MaximeRougieux <maximer@bam.tech>
2023-07-25 14:43:27 +0200

    feat: :sparkles: Dojo Carousel Part 2: Done

--- lib/rounded carousel/team_1.dart
+++ lib/rounded carousel/team_1.dart
@@ -1,8 +1,8 @@
 import 'package:bam_dojo/helpers/team_class.dart';
 import 'package:bam_dojo/rounded%20carousel/rounded%20carousel.dart';
-import 'package:collection/collection.dart';
 
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class RoundedCarouselTeam1 extends StatefulWidget with TeamMixin {
   final teamName = 'Team1';
@@ -13,6 +13,17 @@ class RoundedCarouselTeam1 extends StatefulWidget with TeamMixin {
 
 class _RoundedCarouselTeam1State extends State<RoundedCarouselTeam1> {
   final _controller = PageController(viewportFraction: 0.6);
+  var _currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller.addListener(() {
+      setState(() {
+        _currentIndex = _controller.page!.round();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +31,64 @@ class _RoundedCarouselTeam1State extends State<RoundedCarouselTeam1> {
       color: Colors.white,
       home: Scaffold(
         backgroundColor: Colors.white,
-        body: PageView(
-          controller: _controller,
-          children: roundedCarouselImages
-              .mapIndexed(
-                (index, imageUrl) => Center(
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(100),
-                    child: Image.network(
-                      imageUrl,
+        body: Stack(
+          fit: StackFit.expand,
+          children: [
+            Positioned.fill(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(
+                    child: Container(
+                      color: Colors.white,
                     ),
                   ),
-                ),
-              )
-              .toList(),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                          color: Colors.pink[100],
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.elliptical(
+                                MediaQuery.of(context).size.height / 4, 70),
+                            topRight: Radius.elliptical(
+                                MediaQuery.of(context).size.height / 4, 70),
+                          )),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Center(
+              child: PageView.builder(
+                controller: _controller,
+                itemBuilder: (context, index) {
+                  return Center(
+                    child: AnimatedContainer(
+                      transform: Matrix4.translation(
+                        vector.Vector3(
+                          0.0,
+                          index == _currentIndex ? -80 : 40,
+                          0.0,
+                        ),
+                      ),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(100),
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: NetworkImage(
+                              roundedCarouselImages[index],
+                            ),
+                          )),
+                      duration: const Duration(milliseconds: 300),
+                      height: index == _currentIndex ? 300 : 200,
+                      width: 200,
+                    ),
+                  );
+                },
+                itemCount: roundedCarouselImages.length,
+              ),
+            ),
+          ],
         ),
       ),
     );
