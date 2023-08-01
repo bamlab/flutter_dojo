cee5b239dd2ae5e390f358c81a48c6ee7022113e
MaximeRougieux <maximer@bam.tech>
2023-08-01 11:57:50 +0200

    feat: :sparkles: Add ToolbarItem in all teams

--- lib/interactive_toolbar/team_1.dart
+++ lib/interactive_toolbar/team_1.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:bam_dojo/helpers/team_class.dart';
 
 import 'package:flutter/material.dart';
@@ -6,12 +8,45 @@ class InteractiveToolbarTeam1 extends StatefulWidget with TeamMixin {
   final teamName = 'Team1';
 
   @override
-  State<InteractiveToolbarTeam1> createState() => _InteractiveToolbarTeam1State();
+  State<InteractiveToolbarTeam1> createState() =>
+      _InteractiveToolbarTeam1State();
 }
 
 class _InteractiveToolbarTeam1State extends State<InteractiveToolbarTeam1> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Center(
+      child: _ToolbarItem(),
+    );
   }
 }
+
+class _ToolbarItem extends StatelessWidget {
+  const _ToolbarItem();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        color: randomColor(),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: CircleAvatar(
+        radius: 20,
+        backgroundColor: Colors.transparent,
+        backgroundImage: NetworkImage(
+          'https://avatars.dicebear.com/api/adventurer/${Random().nextInt(1000)}.png',
+        ),
+      ),
+    );
+  }
+}
+
+Color randomColor() => HSLColor.fromAHSL(
+      1,
+      Random().nextDouble() * 360,
+      0.7,
+      0.5,
+    ).toColor();
--- lib/interactive_toolbar/team_3.dart
+++ lib/interactive_toolbar/team_3.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:bam_dojo/helpers/team_class.dart';
 
 import 'package:flutter/material.dart';
@@ -6,12 +8,45 @@ class InteractiveToolbarTeam3 extends StatefulWidget with TeamMixin {
   final teamName = 'Team3';
 
   @override
-  State<InteractiveToolbarTeam3> createState() => _InteractiveToolbarTeam3State();
+  State<InteractiveToolbarTeam3> createState() =>
+      _InteractiveToolbarTeam3State();
 }
 
 class _InteractiveToolbarTeam3State extends State<InteractiveToolbarTeam3> {
   @override
   Widget build(BuildContext context) {
-    return SizedBox();
+    return Center(
+      child: _ToolbarItem(),
+    );
   }
 }
+
+class _ToolbarItem extends StatelessWidget {
+  const _ToolbarItem();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        color: randomColor(),
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: CircleAvatar(
+        radius: 20,
+        backgroundColor: Colors.transparent,
+        backgroundImage: NetworkImage(
+          'https://avatars.dicebear.com/api/adventurer/${Random().nextInt(1000)}.png',
+        ),
+      ),
+    );
+  }
+}
+
+Color randomColor() => HSLColor.fromAHSL(
+      1,
+      Random().nextDouble() * 360,
+      0.7,
+      0.5,
+    ).toColor();
