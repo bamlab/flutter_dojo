04eff132c6da8267401f2b049a4d6a74178c2247
MaximeRougieux <maximer@bam.tech>
2023-08-01 14:15:03 +0200

    feat: :sparkles: Interactive Toolbar : Dojo 1 done

--- lib/interactive_toolbar/team_2.dart
+++ lib/interactive_toolbar/team_2.dart
@@ -13,33 +13,88 @@ class InteractiveToolbarTeam2 extends StatefulWidget with TeamMixin {
 }
 
 class _InteractiveToolbarTeam2State extends State<InteractiveToolbarTeam2> {
+  final _scrollController = ScrollController();
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: _ToolbarItem(),
+    return MaterialApp(
+      home: Scaffold(
+        body: Padding(
+          padding: const EdgeInsets.only(left: 24),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: SizedBox(
+              height: 450,
+              width: 70,
+              child: Card(
+                margin: EdgeInsets.zero,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                elevation: 20,
+                child: ListView.builder(
+                  controller: _scrollController,
+                  padding: EdgeInsets.zero,
+                  itemBuilder: (context, index) {
+                    return _ToolbarItem();
+                  },
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
 
-class _ToolbarItem extends StatelessWidget {
-  const _ToolbarItem();
+class _ToolbarItem extends StatefulWidget {
+  _ToolbarItem();
+
+  final color = randomColor();
+  final imageUrl =
+      'https://avatars.dicebear.com/api/adventurer/${Random().nextInt(1000)}.png';
 
   @override
+  State<_ToolbarItem> createState() => _ToolbarItemState();
+}
+
+class _ToolbarItemState extends State<_ToolbarItem> {
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 50,
-      width: 50,
-      decoration: BoxDecoration(
-        color: randomColor(),
-        borderRadius: BorderRadius.circular(8),
-      ),
-      child: CircleAvatar(
-        radius: 20,
-        backgroundColor: Colors.transparent,
-        backgroundImage: NetworkImage(
-          'https://avatars.dicebear.com/api/adventurer/${Random().nextInt(1000)}.png',
-        ),
-      ),
+    return TweenAnimationBuilder<double>(
+      duration: Duration(milliseconds: 300),
+      tween: Tween(begin: 1 / 5, end: 1),
+      builder: (context, value, child) {
+        final avatarRadius = 20 * value;
+
+        return SizedBox(
+          height: 70,
+          width: 70,
+          child: Padding(
+            padding: EdgeInsets.all(35 - avatarRadius * 5 / 4),
+            child: DecoratedBox(
+              decoration: BoxDecoration(
+                color: widget.color,
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Padding(
+                padding: EdgeInsets.all(avatarRadius * 1 / 4),
+                child: CircleAvatar(
+                  radius: avatarRadius,
+                  backgroundColor: Colors.transparent,
+                  child: Image.network(
+                    widget.imageUrl,
+                    fit: BoxFit.cover,
+                    alignment: Alignment.center,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+      child: SizedBox(),
     );
   }
 }
