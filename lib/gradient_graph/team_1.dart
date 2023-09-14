e235e6a5448dd3e472acfcc689369e587f1c9dc4
Paul <paulb@bam.tech>
2023-09-14 18:18:38 +0200

    wip

--- lib/gradient_graph/team_1.dart
+++ lib/gradient_graph/team_1.dart
@@ -45,11 +45,14 @@ class _GradientGraphTeam1State extends State<GradientGraphTeam1> {
       home: Scaffold(
         backgroundColor: Colors.white,
         body: Center(
-          child: Container(
-            height: 300,
-            width: 300,
-            child: CustomPaint(
-                painter: GraphPainter(dataPoints: normalizedDataPoints)),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            child: Container(
+              height: 300,
+              width: dataPoints.length.toDouble() * 5,
+              child: CustomPaint(
+                  painter: GraphPainter(dataPoints: normalizedDataPoints)),
+            ),
           ),
         ),
       ),
@@ -62,18 +65,71 @@ class GraphPainter extends CustomPainter {
   final IList<DataPoint> dataPoints;
   @override
   void paint(Canvas canvas, Size size) {
-    Paint paint = Paint()
-      ..color = Colors.red
-      ..strokeWidth = 2
-      ..strokeCap = StrokeCap.round;
+    Paint paintStroke = Paint()
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 6
+        // ..strokeCap = StrokeCap.round;
+        ;
+    paintStroke.shader = LinearGradient(
+      colors: [
+        Colors.deepOrange[200]!,
+        Colors.deepOrange[300]!,
+      ],
+      begin: Alignment.centerLeft,
+      end: Alignment.centerRight,
+    ).createShader(Rect.fromLTWH(0, 0, size.width, size.height));
 
-    final path = Path()..moveTo(0, dataPoints[0].value * size.height);
-    for (int i = 1; i < dataPoints.length; i++) {
-      path.lineTo(i * size.width / (dataPoints.length - 1),
-          dataPoints[i].value * size.height);
+    Paint paintShader = Paint()..style = PaintingStyle.fill;
+    paintShader.shader = LinearGradient(
+      colors: [
+        Colors.deepOrange[100]!,
+        Colors.white,
+      ],
+      begin: Alignment.topCenter,
+      end: Alignment.bottomCenter,
+    ).createShader(Rect.fromLTWH(0, 0, size.width, size.height));
+
+    final pathShader = Path()..moveTo(0, size.height);
+
+    for (int i = 0; i < dataPoints.length - 1; i++) {
+      final point = Offset(i * size.width / (dataPoints.length - 2),
+          size.height - dataPoints[i].value * size.height);
+      final point2 = Offset((i + 1) * size.width / (dataPoints.length - 2),
+          size.height - dataPoints[i + 1].value * size.height);
+
+      final double xc = (point.dx + point2.dx) / 2;
+      final double yc = (point.dy + point2.dy) / 2;
+      pathShader.quadraticBezierTo(
+        point.dx,
+        point.dy,
+        xc,
+        yc,
+      );
     }
 
-    canvas.drawPath(path, paint);
+    pathShader.lineTo(size.width, size.height);
+
+    canvas.drawPath(pathShader, paintShader);
+    final pathStroke = Path()
+      ..moveTo(0, size.height - dataPoints[0].value * size.height);
+
+    for (int i = 0; i < dataPoints.length - 1; i++) {
+      final point = Offset(i * size.width / (dataPoints.length - 2),
+          size.height - dataPoints[i].value * size.height);
+      final point2 = Offset((i + 1) * size.width / (dataPoints.length - 2),
+          size.height - dataPoints[i + 1].value * size.height);
+
+      final double xc = (point.dx + point2.dx) / 2;
+      final double yc = (point.dy + point2.dy) / 2;
+      pathStroke.quadraticBezierTo(
+        point.dx,
+        point.dy,
+        xc,
+        yc,
+      );
+    }
+
+    canvas.drawPath(pathStroke, paintStroke);
   }
 
   @override
