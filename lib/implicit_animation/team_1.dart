d673e049fca67fe4fc7daa29372b63c03254a645
MaximeRougieux <maximer@bam.tech>
2023-10-23 16:47:55 +0200

    feat: :sparkles: Implicit animation : Dojo 1 Done

--- lib/implicit_animation/team_1.dart
+++ lib/implicit_animation/team_1.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bam_dojo/helpers/team_class.dart';
 
 import 'package:flutter/material.dart';
@@ -10,15 +12,47 @@ class ImplicitAnimationTeam1 extends StatefulWidget with TeamMixin {
 }
 
 class _ImplicitAnimationTeam1State extends State<ImplicitAnimationTeam1> {
+  double opacity = 0;
+  double scale = 1;
+  double turns = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Icon(
-            Icons.favorite_border_outlined,
-            color: Colors.grey,
-            size: 64,
+          child: GestureDetector(
+            onTap: () => setState(() {
+              opacity = 1 - opacity;
+              scale = scale == 1 ? 2 : 1;
+              turns = 1 - turns;
+            }),
+            child: AnimatedRotation(
+              duration: const Duration(milliseconds: 500),
+              turns: turns,
+              child: AnimatedScale(
+                duration: const Duration(milliseconds: 500),
+                scale: scale,
+                child: Stack(
+                  children: [
+                    AnimatedOpacity(
+                      opacity: opacity,
+                      duration: const Duration(milliseconds: 500),
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                        size: 64,
+                      ),
+                    ),
+                    Icon(
+                      Icons.favorite_border_outlined,
+                      color: Colors.grey,
+                      size: 64,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
