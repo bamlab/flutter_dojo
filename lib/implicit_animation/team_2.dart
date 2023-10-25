0f67093745ba4733d3ff1101ff04ce14f3aab614
matthias-prevost <matthias.prevost@gmail.com>
2023-10-25 12:52:01 +0200

    dojo implicites animations 2 : team 2

--- lib/implicit_animation/team_2.dart
+++ lib/implicit_animation/team_2.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:bam_dojo/helpers/team_class.dart';
 
 import 'package:flutter/material.dart';
@@ -9,16 +11,68 @@ class ImplicitAnimationTeam2 extends StatefulWidget with TeamMixin {
   State<ImplicitAnimationTeam2> createState() => _ImplicitAnimationTeam2State();
 }
 
-class _ImplicitAnimationTeam2State extends State<ImplicitAnimationTeam2> {
+class _ImplicitAnimationTeam2State extends State<ImplicitAnimationTeam2>
+    with TickerProviderStateMixin {
+  double opacity = 0;
+  double scale = 1;
+  double turns = 0;
+  late final AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 1000),
+    );
+  }
+
+  late final Animation<double> _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.linear,
+  );
+
+  late final Animation<double> _blink = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.bounceIn,
+  );
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Icon(
-            Icons.favorite_border_outlined,
-            color: Colors.grey,
-            size: 64,
+          child: GestureDetector(
+            onTap: () => setState(() {
+              opacity = 1 - opacity;
+              scale = scale == 1 ? 2 : 1;
+              turns = 1 - turns;
+              opacity == 1 ? _controller.repeat() : _controller.reverse();
+            }),
+            child: RotationTransition(
+              turns: _animation,
+              child: AnimatedScale(
+                duration: const Duration(milliseconds: 500),
+                scale: scale,
+                child: Stack(
+                  children: [
+                    FadeTransition(
+                      opacity: _blink,
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                        size: 64,
+                      ),
+                    ),
+                    Icon(
+                      Icons.favorite_border_outlined,
+                      color: Colors.grey,
+                      size: 64,
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
         ),
       ),
