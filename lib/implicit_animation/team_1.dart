3edaa5099e04b01398a7d42b38e5d1fc0e62144a
MaximeRougieux <maximer@bam.tech>
2023-10-31 18:01:39 +0100

    feat: :sparkles: Implicit animation: Dojo done

--- lib/implicit_animation/team_1.dart
+++ lib/implicit_animation/team_1.dart
@@ -1,5 +1,3 @@
-import 'dart:async';
-
 import 'package:bam_dojo/helpers/team_class.dart';
 
 import 'package:flutter/material.dart';
@@ -11,47 +9,53 @@ class ImplicitAnimationTeam1 extends StatefulWidget with TeamMixin {
   State<ImplicitAnimationTeam1> createState() => _ImplicitAnimationTeam1State();
 }
 
-class _ImplicitAnimationTeam1State extends State<ImplicitAnimationTeam1> {
-  double opacity = 0;
-  double scale = 1;
-  double turns = 0;
+class _ImplicitAnimationTeam1State extends State<ImplicitAnimationTeam1>
+    with TickerProviderStateMixin {
+  late final _controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 2000),
+  )..repeat(reverse: true);
+
+  late final _animation = CurvedAnimation(
+    parent: _controller,
+    curve: Curves.easeInOut,
+  );
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: GestureDetector(
-            onTap: () => setState(() {
-              opacity = 1 - opacity;
-              scale = scale == 1 ? 2 : 1;
-              turns = 1 - turns;
-            }),
-            child: AnimatedRotation(
-              duration: const Duration(milliseconds: 500),
-              turns: turns,
-              child: AnimatedScale(
-                duration: const Duration(milliseconds: 500),
-                scale: scale,
-                child: Stack(
-                  children: [
-                    AnimatedOpacity(
-                      opacity: opacity,
-                      duration: const Duration(milliseconds: 500),
-                      child: Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                        size: 64,
-                      ),
-                    ),
-                    Icon(
-                      Icons.favorite_border_outlined,
-                      color: Colors.grey,
-                      size: 64,
-                    ),
-                  ],
+          child: RotationTransition(
+            turns: _animation,
+            child: Stack(
+              children: [
+                AnimatedBuilder(
+                  builder: (context, child) {
+                    return Opacity(
+                      opacity: _animation.value > 0.5 ? 1 : 0,
+                      child: child,
+                    );
+                  },
+                  animation: _animation,
+                  child: Icon(
+                    Icons.favorite,
+                    color: Colors.red,
+                    size: 64,
+                  ),
                 ),
-              ),
+                Icon(
+                  Icons.favorite_border_outlined,
+                  color: Colors.grey,
+                  size: 64,
+                ),
+              ],
             ),
           ),
         ),
