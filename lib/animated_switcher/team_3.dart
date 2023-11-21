5eb07fdf167c497cb8a86f90601a783193c502c7
Thomas Coumau <thomasco@bam.tech>
2023-11-21 16:53:57 +0100

    animated switcher

--- lib/animated_switcher/team_3.dart
+++ lib/animated_switcher/team_3.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:bam_dojo/helpers/team_class.dart';
 
 import 'package:flutter/material.dart';
@@ -10,16 +12,42 @@ class AnimatedSwitcherTeam3 extends StatefulWidget with TeamMixin {
 }
 
 class _AnimatedSwitcherTeam3State extends State<AnimatedSwitcherTeam3> {
+  Color color = Colors.red;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () => setState(() {
+                color = Color((Random().nextDouble() * 0xFFFFFF).toInt())
+                    .withOpacity(1.0);
+              }),
+              child: Text('hello'),
+            ),
+            Center(
+              child: AnimatedSwitcher(
+                transitionBuilder: (child, animation) =>
+                    SizeTransition(sizeFactor: animation, child: child),
+                layoutBuilder: (currentChild, previousChildren) => Column(
+                  children: [
+                    ...previousChildren,
+                    if (currentChild != null) currentChild,
+                  ],
+                ),
+                duration: Duration(seconds: 1),
+                child: Container(
+                  key: ValueKey(color),
+                  width: 100,
+                  height: 100,
+                  color: color,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
