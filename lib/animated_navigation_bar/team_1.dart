8922a3b41e19dfc820049a6be540ff5e4c13f510
Paul <paulb@bam.tech>
2024-01-30 17:27:07 +0100

    AnimatedNavigationBar(team1): des tests

--- lib/animated_navigation_bar/team_1.dart
+++ lib/animated_navigation_bar/team_1.dart
@@ -10,30 +10,174 @@ class AnimatedNavigationBarTeam1 extends StatefulWidget with TeamMixin {
       _AnimatedNavigationBarTeam1State();
 }
 
-class _AnimatedNavigationBarTeam1State
-    extends State<AnimatedNavigationBarTeam1> {
+class _AnimatedNavigationBarTeam1State extends State<AnimatedNavigationBarTeam1>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller = AnimationController(
+    duration: const Duration(milliseconds: 150),
+    vsync: this,
+  );
+  late Tween<double> _tween = Tween(begin: 0.0, end: 0.0);
+
   var index = 0;
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    void _onItemTapped(int newIndex) {
+      setState(() {
+        _tween = Tween(
+            begin: _tween.evaluate(_controller), end: newIndex.toDouble());
+        index = newIndex;
+      });
+      _controller.forward(from: 0.0);
+    }
+
+    final width = MediaQuery.of(context).size.width;
+    final icon = switch (index) {
+      0 => Icons.home,
+      1 => Icons.person,
+      2 => Icons.settings,
+      _ => Icons.home,
+    };
+
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: switch (index) {
-          0 => Colors.red,
-          1 => Colors.green,
-          2 => Colors.blue,
-          _ => Colors.cyan,
-        },
-        bottomNavigationBar: BottomNavigationBar(
-          items: [
-            BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-            BottomNavigationBarItem(icon: Icon(Icons.person), label: 'Profile'),
-            BottomNavigationBarItem(
-                icon: Icon(Icons.settings), label: 'Settings'),
-          ],
-          currentIndex: index,
-          onTap: (value) => setState(() => index = value),
+      home: Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Colors.red,
+              Colors.blue,
+            ],
+          ),
+        ),
+        child: Scaffold(
+          backgroundColor: Colors.transparent,
+          bottomNavigationBar: AnimatedBuilder(
+            animation: _controller,
+            builder: (context, _) {
+              return Stack(
+                children: [
+                  SizedBox(
+                    height: 130,
+                    child: Align(
+                      alignment: Alignment.bottomCenter,
+                      child: ClipPath(
+                        clipper: BottomNavClipper(
+                            animationValue: _tween.evaluate(_controller)),
+                        child: BottomNavigationBar(
+                          iconSize: 44,
+                          items: [
+                            BottomNavigationBarItem(
+                              icon: Icon(Icons.home),
+                              label: index == 0 ? '' : 'Home',
+                              activeIcon: SizedBox.shrink(),
+                            ),
+                            BottomNavigationBarItem(
+                              icon: Icon(Icons.person),
+                              label: index == 1 ? '' : 'Profile',
+                              activeIcon: SizedBox.shrink(),
+                            ),
+                            BottomNavigationBarItem(
+                              icon: Icon(Icons.settings),
+                              label: index == 2 ? '' : 'Settings',
+                              activeIcon: SizedBox.shrink(),
+                            ),
+                          ],
+                          currentIndex: index,
+                          onTap: _onItemTapped,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Positioned(
+                    left: _tween.evaluate(_controller) * width / 3,
+                    child: SizedBox(
+                      width: width / 3,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.white,
+                        ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: AnimatedSwitcher(
+                            duration: Duration(milliseconds: 150),
+                            transitionBuilder:
+                                (Widget child, Animation<double> animation) {
+                              return FadeTransition(
+                                opacity: animation,
+                                child: child,
+                              );
+                            },
+                            child: Icon(
+                              key: ValueKey(icon),
+                              icon,
+                              size: 44,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
       ),
     );
+  }
+}
+
+class BottomNavClipper extends CustomClipper<Path> {
+  BottomNavClipper({required this.animationValue});
+
+  final double animationValue;
+  @override
+  Path getClip(Size size) {
+    final path = Path();
+    final height = size.height;
+    final width = size.width;
+    final xStart = animationValue * width / 3;
+    final partLength = width / 6;
+
+    path.lineTo(0, 0);
+    path.lineTo(xStart, 0);
+    final b1 = 0.5;
+    final b2 = 0.1;
+
+    path.cubicTo(
+      xStart + b1 * partLength,
+      0,
+      xStart + partLength * b2,
+      4 * partLength / 5,
+      xStart + partLength,
+      4 * partLength / 5,
+    );
+
+    path.cubicTo(
+      xStart + partLength + partLength * (1 - b2),
+      4 * partLength / 5,
+      xStart + partLength + partLength * (1 - b1),
+      0,
+      xStart + 2 * partLength,
+      0,
+    );
+
+    path.lineTo(width, 0);
+    path.lineTo(width, height);
+    path.lineTo(0, height);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(BottomNavClipper oldClipper) {
+    print(animationValue);
+    return true;
   }
 }
